c9728882fae24c57d855b0ec88243785b4bf2b5c
xXTheSebXx <sebastianjerke@gmail.com>
2024-10-19 18:15:31 +0200

    now works for the bot at server

--- src/main/java/com/seb/musicapp/Main.java
+++ src/main/java/com/seb/musicapp/Main.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 
 public class Main extends Application {
 
-    public static boolean DEBUG = true;
+    public static boolean DEBUG = false;
     public Connector connector;
     public Stage stage;
     public Scene scene;
