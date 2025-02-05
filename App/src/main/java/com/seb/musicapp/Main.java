cd83a2f0587135624e255734d26b1a37dfe89634
xXTheSebXx <sebastianjerke@gmail.com>
2025-02-05 17:03:46 +0100

    StringBuilder instead of Buffer

--- App/src/main/java/com/seb/musicapp/Main.java
+++ App/src/main/java/com/seb/musicapp/Main.java
@@ -190,7 +190,7 @@ public class Main extends Application {
         con.setRequestMethod("GET");
         BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
         String inputLine;
-        StringBuffer content = new StringBuffer();
+        StringBuilder content = new StringBuilder();
         while ((inputLine = in.readLine()) != null) {
             content.append(inputLine);
         }
